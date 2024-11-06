c4994b5334195ba1a1071df38ae5da2f7a454fd7
Erick Giraldo <erickgiraldo94@gmail.com>
2024-11-06 16:51:53 -0500

    feat: implemented shuffle

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -51,4 +52,11 @@ func newDeckFromFile(filename string) deck {
 	}
 
 	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeckFromFile("hand.txt")
+	cards.print()
+	cards.shuffle()
+	fmt.Println("\n")
 	cards.print()
 }
